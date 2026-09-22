13f419431f1ee88218b11da56bb4b003577c278b
MUIBEU <seoghunjo48@gmail.com>
2026-09-22 10:58:29 +0900

    add Unity main scene bootstrap

---
+++ unity/Assets/Editor/ProjectBootstrap.cs
@@ -0,0 +1,58 @@
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CryptoMining.Editor
+{
+    [InitializeOnLoad]
+    public static class ProjectBootstrap
+    {
+        const string ScenePath = "Assets/Scenes/Main.unity";
+
+        static ProjectBootstrap()
+        {
+            EditorApplication.delayCall += EnsureMainScene;
+        }
+
+        [MenuItem("Tools/Crypto Mining/Create Main Scene")]
+        public static void EnsureMainScene()
+        {
+            if (!AssetDatabase.IsValidFolder("Assets/Scenes"))
+                AssetDatabase.CreateFolder("Assets", "Scenes");
+
+            if (!File.Exists(ScenePath))
+            {
+                Scene scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Additive);
+                scene.name = "Main";
+                EditorSceneManager.SaveScene(scene, ScenePath);
+                EditorSceneManager.CloseScene(scene, true);
+                AssetDatabase.Refresh();
+                Debug.Log("Crypto Mining: Assets/Scenes/Main.unity created.");
+            }
+
+            EditorBuildSettingsScene[] current = EditorBuildSettings.scenes;
+            bool found = false;
+            for (int i = 0; i < current.Length; i++)
+                if (current[i].path == ScenePath) found = true;
+
+            if (!found)
+            {
+                EditorBuildSettingsScene[] next = new EditorBuildSettingsScene[current.Length + 1];
+                for (int i = 0; i < current.Length; i++) next[i] = current[i];
+                next[next.Length - 1] = new EditorBuildSettingsScene(ScenePath, true);
+                EditorBuildSettings.scenes = next;
+            }
+        }
+
+        [MenuItem("Tools/Crypto Mining/Open Main Scene")]
+        public static void OpenMainScene()
+        {
+            EnsureMainScene();
+            EditorSceneManager.OpenScene(ScenePath, OpenSceneMode.Single);
+        }
+    }
+}
+#endif
